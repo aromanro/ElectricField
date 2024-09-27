d67dc908d1de2f2a1fc332d57910d344612681f0
Adrian Roman <aroman_ro@yahoo.com>
2024-09-27 12:42:26 +0300

    Minor change to worker thread

--- ElectricField/WorkerThread.h
+++ ElectricField/WorkerThread.h
@@ -6,87 +6,85 @@
 template<class WorkerThreadsPool, class Job> class WorkerThread
 {
 public:
-    explicit WorkerThread(WorkerThreadsPool *threadsPool, bool autostart = false)
-        : m_ThreadsPool(threadsPool), m_Stop(false)
-    {
-        if (autostart) Start();
-    }
+	explicit WorkerThread(WorkerThreadsPool *threadsPool, bool autostart = false)
+		: m_ThreadsPool(threadsPool), m_Stop(false)
+	{
+		if (autostart) Start();
+	}
 
-    ~WorkerThread()
-    {
-        Stop();
-    }
+	~WorkerThread()
+	{
+		Stop();
+	}
 
-    void Start()
-    {
-        {
-            std::lock_guard<std::mutex> lock(m_ThreadsPool->m_Mutex);
-            m_Stop = false;
-        }
-        m_Thread = std::thread(&WorkerThread::Calculate, this);
-    }
+	void Start()
+	{
+		{
+			std::lock_guard<std::mutex> lock(m_ThreadsPool->m_Mutex);
+			m_Stop = false;
+		}
+		m_Thread = std::thread(&WorkerThread::Calculate, this);
+	}
 
-    void Stop()
-    {
-        bool notify = false;
-        {
-            std::lock_guard<std::mutex> lock(m_ThreadsPool->m_Mutex);
-            if (!m_Stop)
-            {
-                m_Stop = true;
-                notify = true;
-            }
-        }
-        if (notify) m_ThreadsPool->NotifyAll();
-        Join();
-    }
+	void Stop()
+	{
+		bool notify = false;
+		{
+			std::lock_guard<std::mutex> lock(m_ThreadsPool->m_Mutex);
+			if (!m_Stop)
+			{
+				m_Stop = true;
+				notify = true;
+			}
+		}
+		if (notify) m_ThreadsPool->NotifyAll();
+		Join();
+	}
 
-    void SetStopUnlocked()
-    {
-        m_Stop = true;
-    }
+	void SetStopUnlocked()
+	{
+		m_Stop = true;
+	}
 
-    inline void Join()
-    {
-        if (m_Thread.joinable()) m_Thread.join();
-    }
+	inline void Join()
+	{
+		if (m_Thread.joinable()) m_Thread.join();
+	}
 
 private:
-    inline bool TerminateWait() const
-    {
-        return m_ThreadsPool->HasWork() || m_Stop;
-    }
+	inline bool TerminateWait() const
+	{
+		return m_ThreadsPool->HasWork() || m_Stop;
+	}
 
-    void Calculate()
-    {
-        do
-        {
-            std::unique_lock<std::mutex> lock(m_ThreadsPool->m_Mutex);
+	void Calculate()
+	{
+		for (;;)
+		{
+			std::unique_lock<std::mutex> lock(m_ThreadsPool->m_Mutex);
 
-            if (!TerminateWait())
-                m_ThreadsPool->m_Condition.wait(lock, [this] { return TerminateWait(); });
+			if (!TerminateWait())
+				m_ThreadsPool->m_Condition.wait(lock, [this] { return TerminateWait(); });
 
-            while (!m_ThreadsPool->m_JobsQueue.empty() && !m_Stop)
-            {
-                Job job = m_ThreadsPool->m_JobsQueue.front();
-                m_ThreadsPool->m_JobsQueue.pop();
-                lock.unlock();
+			while (!m_ThreadsPool->m_JobsQueue.empty() && !m_Stop)
+			{
+				Job job = m_ThreadsPool->m_JobsQueue.front();
+				m_ThreadsPool->m_JobsQueue.pop();
+				lock.unlock();
 
-                job->Calculate();
+				job->Calculate();
 
-                lock.lock();
-            } 
+				lock.lock();
+			} 
 
-            if (m_Stop) break;
+			if (m_Stop) break;
+		}
+	}
 
-            lock.unlock();
-        } while (true);
-    }
+	WorkerThreadsPool* m_ThreadsPool;
 
-    WorkerThreadsPool* m_ThreadsPool;
+	std::thread m_Thread;
 
-    std::thread m_Thread;
-
-    bool m_Stop;
+	bool m_Stop;
 };
 
